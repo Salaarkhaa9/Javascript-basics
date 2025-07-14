c3545f3c9816641d3a0455d9f3bd8d326ae4fad7
Salar Khan <126601551+Salaarkhaa9@users.noreply.github.com>
2025-07-14 15:41:52 +0000

    Constants

--- basics_1/test.js
+++ basics_1/test.js
@@ -1,1 +1,15 @@
 console.log("Salar")
+
+const accountId=12345;
+let accountName="Salar";
+var accountEmail="salarwasil2004@gmail.com";
+accountCity="Rawalpindi";
+let accountState;
+/*
+prefer not to use var cause of block scope
+*/
+accountName="FARHAN";
+accountEmail="abs@gmail.com";
+accountCity="Islamabad";
+
+console.table([accountEmail,accountId,accountName,accountState,accountCity]);
