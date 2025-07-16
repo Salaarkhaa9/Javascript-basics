2da0f2534f9eb9616fea05cc4af1c2b06f1febac
Salaarkhaa9 <kowalskikh92@gmail.com>
2025-07-16 14:52:26 +0500

    strings

---
+++ basics_1/strings.js
@@ -0,0 +1,24 @@
+const name= "Salar";
+const awards= "50";
+
+console.log(`Hello my name is ${name} and my award count is ${awards}`);
+
+const gameName = new String("hitesh-hc");
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+
+console.log(gameName.indexOf('H'));
+console.log(gameName.substring(0,4));
+console.log(gameName);
+
+const newString= gameName.slice(-8,4);
+console.log(newString);
+
+const newName=new String("   hrlllnkjhkjhkj    ")
+console.log(newName.trimStart());
+
+
+
+
