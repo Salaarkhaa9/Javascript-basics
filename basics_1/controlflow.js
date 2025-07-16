3e03417a34ebb40919534060a1a45b067d3c648d
Salaarkhaa9 <kowalskikh92@gmail.com>
2025-07-16 12:28:49 +0500

    switch statement

--- basics_1/controlflow.js
+++ basics_1/controlflow.js
@@ -1,1 +1,14 @@
 console.log("ajakhfksahfkjas")
+
+
+const day = "Monday";
+switch (day) {
+    case "Monday":
+        console.log("Start of the week.");
+        break;
+    case "Friday":
+        console.log("End of the workweek.");
+        break;
+    default:
+        console.log("It's a regular day.");
+}
