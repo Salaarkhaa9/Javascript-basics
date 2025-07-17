4aeccbc63a060a4f98538d030c833a21426767b8
Salaarkhaa9 <kowalskikh92@gmail.com>
2025-07-17 22:19:00 +0500

    update functions

--- 3_functions/functions.js
+++ 3_functions/functions.js
@@ -18,3 +18,32 @@ function loggedInUser(username="khan"){
 console.log(loggedInUser("Salar"));
 
 // console.log(loggedInUser());
+function caclPrice(val1,val2,...num1){
+    return num1
+}
+// console.log(caclPrice(200,300,500,10190));
+
+const user={
+    username:"saalar",
+    email:"adlkfjdalkjflkajfl"
+}
+
+function newObject(anyObject) {
+    console.log(`Hello my name is ${anyObject.username} and my email is ${anyObject.email}`);
+    
+}
+// newObject(user)
+
+newObject({
+    username:"sam",
+    email:"kjahfkjahfkjahf"
+})
+
+const array=[200,300,400,500]
+
+function myArr(newArr){
+    return newArr[1]
+}
+// console.log(myArr(array));
+
+console.log(myArr([200,301,909,55]));
