6b1368687bee62236d4a2c0c0f7a848a0d1dffdd
Salaarkhaa9 <kowalskikh92@gmail.com>
2025-07-20 15:19:43 +0500

    while loop

---
+++ 5_iterations/while.js
@@ -0,0 +1,18 @@
+let index=0
+// while (index<=10) {
+//     console.log(`Value of index is ${index}`);
+//     index=index+2
+// }
+
+let arr=["flasj","sipouyr","basyfs"]
+// let array=0
+// while (array<arr.length) {
+//     console.log(`Value of the item in array is ${arr[array]}`);
+//     array=array+1
+// }
+
+//DO WHILE LOOP
+
+do {
+    
+} while (condition);
